31031f01d4af1b0a0ee1162a1edfc1664b9a5a8c
utsabk <kharelutsab@gmail.com>
2021-07-30 14:35:48 +0300

    Redux-notes - Add new notes - Toggle importance

--- src/index.js
+++ src/index.js
@@ -1,13 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
 
+const noteReducer = (state=[], action) => {
+  switch(action.type){
+    case 'NEW_NOTE':
+      return [...state, action.data]
+     
+    case 'TOGGLE_IMPORTANCE':{
+      const id = action.data.id
+      const noteToChange = state.find(n => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+      return state.map(note => note.id !== id ? note : changedNote)
+    }
 
+    default :
+    return state
+  }
+}
 
+const store = createStore(noteReducer)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: false,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+const App = () =>{
+  return(
+    <div>
+      <ui>
+        {store.getState().map(note => 
+        <li key={note.id}>
+          {note.content}<strong>{note.important ? 'important' : ''}</strong>
+        </li>
+
+        )}
+      </ui>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+renderApp();
+
+store.subscribe(renderApp);
