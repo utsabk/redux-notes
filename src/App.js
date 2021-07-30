e5d1dbeb7b620401f901d2ab593dc9a4004b0279
utsabk <kharelutsab@gmail.com>
2021-07-30 14:06:54 +0300

    Redux Statemangement  - Counter example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
-function App() {
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+
+    case 'DECREMENT':
+      return state - 1;
+
+    case 'ZERO':
+      return 0;
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{store.getState()}</div>
+      <button onClick={(e) => store.dispatch({ type: 'INCREMENT' })}>
+        plus
+      </button>
+      <button onClick={(e) => store.dispatch({ type: 'DECREMENT' })}>
+        minus
+      </button>
+      <button onClick={(e) => store.dispatch({ type: 'ZERO' })}>zero</button>
     </div>
   );
-}
+};
 
-export default App;
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+renderApp();
+
+store.subscribe(renderApp);
