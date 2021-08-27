73c1909d343ae2d29a0a65dd9879d42fb207c009
utsabk <kharelutsab@gmail.com>
2021-08-27 11:54:48 +0300

    State management with redux

--- src/index.js
+++ src/index.js
@@ -1,66 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore} from 'redux';
+import { Provider } from 'react-redux';
+import App from './App';
+import noteReducer from './reducers/noteReducer';
 
-const noteReducer = (state=[], action) => {
-  switch(action.type){
-    case 'NEW_NOTE':
-      return [...state, action.data]
-     
-    case 'TOGGLE_IMPORTANCE':{
-      const id = action.data.id
-      const noteToChange = state.find(n => n.id === id);
-      const changedNote = {
-        ...noteToChange,
-        important: !noteToChange.important
-      }
-      return state.map(note => note.id !== id ? note : changedNote)
-    }
+const store = createStore(noteReducer);
 
-    default :
-    return state
-  }
-}
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
-const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: false,
-    id: 1
-  }
-})
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-const App = () =>{
-  return(
-    <div>
-      <ui>
-        {store.getState().map(note => 
-        <li key={note.id}>
-          {note.content}<strong>{note.important ? 'important' : ''}</strong>
-        </li>
-
-        )}
-      </ui>
-    </div>
-  )
-}
-
-const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-};
-
-renderApp();
-
-store.subscribe(renderApp);
